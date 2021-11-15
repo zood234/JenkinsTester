ad591d1554b14301af87a578a86bd4a4c84f7363
zainm <zainmahmood@live.co.uk>
2021-11-15 12:58:11 +0000

    inital commit

--- app/src/androidTest/java/com/example/jenkinstester/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/jenkinstester/ExampleInstrumentedTest.kt
@@ -13,12 +13,11 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+//
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.jenkinstester", appContext.packageName)
-    }
+//    @Test
+//    fun useAppContext() {
+//        // Context of the app under test.
+//        assertEquals(7, 4+3)
+//    }
 }
